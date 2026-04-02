9b44a17583dfb92776c154d553e2c374f0371060
Diego Lorca <diego.lorcaveliz@gmail.com>
2026-04-02 13:19:58 -0300

    conn usa variables de entorno

--- funciones/conn/conn.php
+++ funciones/conn/conn.php
@@ -4,22 +4,21 @@ function conn()
 {
     ##################### CONEXION #######################################
 
-    $host = 'db';
-    $user =  'vetmind_dev_user';
-    $pass = '*t8@%6Q3TG4--Dalv89--rMhW87moo';
-    $name = 'vetmind_dev_db';
+    $host = getenv('DB_HOST') ?: 'db';
+    $user = getenv('DB_USERNAME') ?: '';
+    $pass = getenv('DB_PASSWORD') ?: '';
+    $name = getenv('DB_DATABASE') ?: '';
 
     $mysqli = new mysqli($host, $user, $pass, $name);
-
-    // Configurar la conexión para usar UTF-8
-    $mysqli->set_charset("utf8");
 
     if ($mysqli->connect_error) {
         die('Error de Conexión (' . $mysqli->connect_errno . ') '
             . $mysqli->connect_error);
     }
 
+    // Configurar la conexión para usar UTF-8
+    $mysqli->set_charset("utf8");
+
     return $mysqli;
 }
-
 ?>
